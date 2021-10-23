ebfe6d15a4fe6f39ae04f4ada0ba2bf9f8e3a30e
unknown <alexrugina8@yahoo.com>
2021-10-23 22:35:05 +0300

    Working...

---
+++ Domain/Obiecte.py
@@ -0,0 +1,48 @@
+def creeaza_obiect(id, nume, descriere, pret, locatie):
+    '''
+    Creeaza obiect
+    :param id: string
+    :param nume: string
+    :param descriere: string
+    :param pret: float
+    :param locatie: string
+    :return: Returneaza un dictionar ce retine un obiect.
+    '''
+
+    return {
+        'id' : id,
+        'nume' : nume,
+        'descriere' : descriere,
+        'pret' : pret,
+        'locatie' : locatie
+
+    }
+
+def get_id(obiect):
+    '''
+    Ia id-ul obiectului
+    :param obiect: dictionar de tip obiect
+    :return: id-ul obiectului
+    '''
+    return obiect['id']
+
+def get_nume(obiect):
+    return obiect['nume']
+
+def get_descriere(obiect):
+    return obiect['descriere']
+
+def get_pret(obiect):
+    return obiect['pret']
+
+def get_locatie(obiect):
+    return obiect ['locatie']
+
+def to_string(obiect):
+    return "id: {}, nume: {}, descriere: {}, pret: {}, locatie: {}".format(
+        get_id(obiect),
+        get_nume(obiect),
+        get_descriere(obiect),
+        get_pret(obiect),
+        get_locatie(obiect)
+    )
